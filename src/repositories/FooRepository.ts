cfeeb6dd91d37823c66dfceac1b7538ecf478a9f
Israel Junior <junior.camargo11@gmail.com>
2023-03-30 11:33:21 -0300

    add interfaces in project

--- src/repositories/FooRepository.ts
+++ src/repositories/FooRepository.ts
@@ -1,5 +1,10 @@
-export class FooRepository {
-  public index(phrase: string) {
+export interface IFooRepository {
+  index(phrase?: string): string;
+}
+export class FooRepository implements IFooRepository {
+  public index(phrase: string): string {
     return `Hellow World. This is my node template api. ${phrase}`;
   }
 }
+
+export default new FooRepository();
--- src/services/FooService.ts
+++ src/services/FooService.ts
@@ -1,14 +1,14 @@
-import { FooRepository } from '../repositories/FooRepository';
+import FooRepository, { IFooRepository } from '../repositories/FooRepository';
 
 export interface IFooService {
   index(phrase?: string): Promise<string>;
 }
-class FooService implements IFooService {
-  constructor(private readonly repository?: FooRepository) {}
+export class FooService implements IFooService {
+  constructor(private readonly repository?: IFooRepository) {}
 
   public async index(phrase?: string): Promise<string> {
     return this.repository.index(phrase ?? '');
   }
 }
 
-export default new FooService(new FooRepository());
+export default new FooService(FooRepository);
